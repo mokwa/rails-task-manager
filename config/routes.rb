1a016a26606754403cb92c9190b7571d05861a04
Juliana Mokwa <julimokwa@gmail.com>
2021-02-16 16:16:13 +0000

    Rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index'
   # As a user, I can add a new task
-  get '/tasks/new', to: 'tasks#new'
+  get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   # As a user, I can view the details of a task
-  get '/tasks', to: 'tasks#index'
-  get '/tasks:id', to: 'tasks#show'
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
   # As a user, I can edit a task (mark as completed / update title & details)
-  get '/tasks/:id/edit', to: 'tasks#edit'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch '/tasks/:id', to: 'tasks#update'
   # As a user, I can remove a task
-  delete '/tasks/:id', to: 'tasks#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
